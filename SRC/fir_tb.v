44a743a7322b3290d5af9f1a9e4cdabec488d7da
RuiqingLuo2023 <125855856+RuiqingLuo2023@users.noreply.github.com>
2025-04-25 11:07:36 +0800

    Update fir_tb.v

--- SRC/fir_tb.v
+++ SRC/fir_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/20/2023 10:38:55 AM
-// Design Name: 
-// Module Name: fir_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module fir_tb
 #(  parameter pADDR_WIDTH = 12,

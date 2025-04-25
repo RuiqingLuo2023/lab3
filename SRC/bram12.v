2a2c1294a324263958e7b12d440d90c68f6e0432
RuiqingLuo2023 <125855856+RuiqingLuo2023@users.noreply.github.com>
2025-04-25 11:06:20 +0800

    Update bram12.v

--- SRC/bram12.v
+++ SRC/bram12.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/03/09 23:59:43
-// Design Name: 
-// Module Name: bram12
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module bram12 
 (
     CLK,
